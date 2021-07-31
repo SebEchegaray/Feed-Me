d60a26b5211cdcdf437a0ee5807e9dd8c0d8d56e
adam <adamgonlag@gmail.com>
2021-07-31 18:42:52 +1000

    sessions now working

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -1,30 +1,32 @@
-const express = require('express');
+const express = require("express");
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 
-router.post('/', (req, res)=>{
-    const email = req.body.email
-    const password = req.body.password
-    User.get(email).then((user) => {
-        req.session.userId = user.id;
-        res.send(req.session)
-    });
-    req.session // Session object
-    // if(user){
-    //      // 
-    // }
+// Login
+router.post("/", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-    // // To check if the user is logged in
-    // if(req.session.userId){
-
-    // }
-    
-
+  User.findByEmail(email).then((user) => {
+    if (user && bcrypt.compareSync(password, user.password_digest)) {
+      req.session.userId = user.id;
+      res.json(req.session);
+    } else {
+      res.status(401).json({ error: "No user found or incorrect password" });
+    }
+  });
 });
 
-router.get('/', (req, res)=>{
-    res.json(req.session); // Or some version of this // Depending on what
-    // you actually want the front end to know about the session
-})
+// Logout
+router.delete("/", (req, res) => {
+  req.session.destroy();
+  res.json({ msg: "User successfully logged out." });
+});
+
+// Get current session
+router.get("/", (req, res) => {
+  res.json(req.session);
+});
 
 module.exports = router;
--- models/user.js
+++ models/user.js
@@ -2,41 +2,32 @@ const db = require("../db/db");
 const bcrypt = require("bcrypt");
 
 const User = {
-    create(name, email, password) {
-        const password_digest = bcrypt.hashSync(
-            password,
-            bcrypt.genSaltSync(10)
-        );
+  create(name, email, password) {
+    const password_digest = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
-        const sql = `
+    const sql = `
         INSERT INTO users(name, email, password_digest)
         VALUES($1, $2, $3)
         RETURNING *
     `;
 
-        //db.query is an async function and returns a promise object
+    return db.query(sql, [name, email, password_digest]).then((dbResult) => {
+      return dbResult.rows[0];
+    });
+  },
 
-        const user = db
-            .query(sql, [name, email, password_digest])
-            .then((dbResult) => {
-                return dbResult.rows[0];
-            });
-
-        return user;
-    },
-
-    get(email) {
-        const sql = `
+  findByEmail(email) {
+    const sql = `
         SELECT * FROM users WHERE email = $1
     `;
-        const user = db.query(sql, [email]).then((dbResult) => {
-            return dbResult.rows[0];
-        });
+    const user = db.query(sql, [email]).then((dbResult) => {
+      return dbResult.rows[0];
+    });
 
-        return user;
-    },
-    update() {}, //TODO
-    delete() {}, //TODO
+    return user;
+  },
+  update() {}, //TODO
+  delete() {}, //TODO
 };
 
 module.exports = User;
